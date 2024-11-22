e35de0b34ff649e9f2f4519dfcab100ec23a24f2
samuelm235 <108957962+samuelm235@users.noreply.github.com>
2024-11-22 14:30:39 +0000

    part b test case

--- Main.java
+++ Main.java
@@ -5,13 +5,23 @@ public class Main
         boolean[][] schedule = new boolean[8][60]; //9 periods, 60 mins each
         AppointmentBook a = new AppointmentBook(schedule);
         System.out.println(a);
-        for(int i = 25; i < 30; i++) schedule[1][i] = true; //go through every min (i) in period two, set to true
+        for(int i = 10; i < 15; i++) schedule[1][i] = true;  
+        for(int i = 30; i < 45; i++) schedule[1][i] = true;
+        for(int i = 50; i < 60; i++) schedule[1][i] = true;
+
+        a.printPeriod(2);
+        System.out.println(a.findFreeBlock(2, 15));
+        System.out.println(a.findFreeBlock(2, 9));
+        System.out.println(a.findFreeBlock(2, 20));
+
+        boolean[][] schedule2 = new boolean[8][60]; //9 periods, 60 mins each
+        AppointmentBook b = new AppointmentBook(schedule2);
+        System.out.println(b);
+        for(int i = 25; i < 30; i++) schedule[1][i] = true;  
         for(int i = 0; i < 15; i++) schedule[2][i] = true;
         for(int i = 41; i < 60; i++) schedule[2][i] = true;
-        for(int i = 5; i < 30; i++) schedule[3][i] = true;
+        or(int i = 5; i < 30; i++) schedule[3][i] = true;
         for(int i = 44; i < 60; i++) schedule[3][i] = true;
-        a.printPeriod(4);
-        System.out.println(a.makeAppointment(2, 4, 22));
-        a.printPeriod(4);
+
     }
 }
