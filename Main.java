57e8e5e81473c09875430aed0cd386ce9aa2e169
samuelm235 <108957962+samuelm235@users.noreply.github.com>
2024-11-27 14:08:59 +0000

    test cases

--- Main.java
+++ Main.java
@@ -16,12 +16,16 @@ public class Main
 
         boolean[][] schedule2 = new boolean[8][60]; //9 periods, 60 mins each
         AppointmentBook b = new AppointmentBook(schedule2);
-        System.out.println(b);
-        for(int i = 25; i < 30; i++) schedule[1][i] = true;  
-        for(int i = 0; i < 15; i++) schedule[2][i] = true;
-        for(int i = 41; i < 60; i++) schedule[2][i] = true;
-        or(int i = 5; i < 30; i++) schedule[3][i] = true;
-        for(int i = 44; i < 60; i++) schedule[3][i] = true;
-
+        for(int i = 25; i < 30; i++) schedule2[1][i] = true;  
+        for(int i = 0; i < 15; i++) schedule2[2][i] = true;
+        for(int i = 41; i < 60; i++) schedule2[2][i] = true;
+        for(int i = 5; i < 30; i++) schedule2[3][i] = true;
+        for(int i = 44; i < 60; i++) schedule2[3][i] = true;
+        System.out.println(b.makeAppointment(2, 4, 22));
+        System.out.println(b.makeAppointment(3, 4, 3));
+        System.out.println(b.makeAppointment(2, 4, 30));
+        b.printPeriod(2);
+        b.printPeriod(3);
+        b.printPeriod(4);
     }
 }
